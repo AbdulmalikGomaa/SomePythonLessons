4b16d7c86ee2c0a4788e612c5a55d237a197e331
١٧٤٩ <109435348+AbdulmalikGomaa@users.noreply.github.com>
2023-02-21 06:52:52 +0200

    Update Funny Progress Bar Lol.py

--- Funny Progress Bar Lol.py
+++ Funny Progress Bar Lol.py
@@ -6,7 +6,7 @@ segment3 = "—"
 segment4 = "\\"
 
 
-def progressbar(iterationCounter):
+def progressbar():
     print(f"\r{segment1}", end="")
     sleep(0.1)
     print(f"\r{segment2}", end="")
@@ -15,12 +15,8 @@ def progressbar(iterationCounter):
     sleep(0.1)
     print(f"\r{segment4}", end="")
     sleep(0.1)
-    iterationCounter += 1
-    if iterationCounter > 700:
-        print("\nget kicked in the ass")
-        iterationCounter = 0
-    progressbar(iterationCounter)
+    progressbar()
 
-progressbar(0)
+progressbar()
 
 #it was meant to be a progressbar but it failed.
